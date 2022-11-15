7647ff01b8e3349108ac837aff449ab86ae7e400
hes <h456ad@gmail.com>
2022-11-15 22:09:12 +0200

    newupdate

--- js/index.js
+++ js/index.js
@@ -1,21 +1,292 @@
+const data = {
+  siteName: "azzrak",
+  icon: "./asset/azrak.png",
+  header:
+    "أزرق .. المنصة الأكبر في إنشاء وإدارة المتاجر الالكترونية في الوطن العربي",
+  description:
+    "نجاح تجارتك الالكترونية اصبح امر عادي بالنسبة لأي شركة لكن كونك ناجح لم يعد كافيا .. أزرق لا تتكلم عن جعلك جيد او ناجح أزرق تحدثك عن انطلاقك نحو السماء",
+  partners: [
+    "./asset/partners/p1.png",
+    "./asset/partners/p2.png",
+    "./asset/partners/p3.png",
+    "./asset/partners/p4.png",
+    "./asset/partners/p5.png",
+  ],
+  results: [
+    "./asset/result/1.png",
+    "./asset/result/2.png",
+    "./asset/result/3.png",
+    "./asset/result/4.png",
+    "./asset/result/5.png",
+  ],
+  steps: [
+    {
+      step: "تواصل معنا الآن",
+      img: "./asset/steps/step1.png",
+    },
+    {
+      step: "معرفة سيستم أزرق",
+      img: "./asset/steps/step2.png",
+    },
+    {
+      step: "الموافقة على الخطة",
+      img: "./asset/steps/step3.png",
+    },
+    {
+      step: "مرحلة جني الأرباح",
+      img: "./asset/steps/step4.png",
+    },
+  ],
+  services: [
+    {
+      serv: "فريق الميديا باينج",
+      des: "الفريق المسؤول عن ادارة الحملات الاعلانية المختلفة مع ضمان تحقيق أفضل النتائج بأقل تكلفة",
+      img: "./asset/serv/ads-1.png",
+    },
+    {
+      serv: "فريق الإنشاء",
+      des: "فريق مكون من كوادر محترفة وفي هذا القسم يتم إستلام خطة العمل الخاصة بالمتجر من فريق المحللين والبدء في تنفيذها بكل المعايير المتوافقة مع محركات البحث أيا كانت المنصة المختارة وباحدث المعايير البرمجية",
+      img: "./asset/serv/ads-1.png",
+    },
+    {
+      serv: "فريق السيو",
+      des: "وهنا يتم تحليل موقعك بالكامل ودراسته ودراسة منافسيه ودراسة السوق و وضع الخطط المصنوعة خصيصا لموقعك او متجرك الالكتروني مع التغيير المستمر للخطط وبناءا على تغييرات ومتطلبات السوق",
+      img: "./asset/serv/ads-1.png",
+    },
+    {
+      serv: "فريق الجرافيك",
+      des: "التصميم الجيد لمتجرك يجعل الأمر أسهل بالنسبة لجوجل أن يتعرف على متجرك ويجعله في الصدارة , ناهيك أن المتجر الذي يوفر منتجات عالية الجودة وبأسعار تنافسية لن يصبح رقم واحد طالما تصميمات منتجاته عقيمة لا تجذب الزائر",
+      img: "./asset/serv/ads-1.png",
+    },
+    {
+      serv: "فريق السوشيال ميديا",
+      des: "فريق محترف في إدارة السوشيال ميديا بشكل كامل و ادارة الاعلانات الممولة على منصات السوشيال ميديا بجميع انواعها وكتابة المحتوى وانشاء التصميمات الخاصة بالمحتوى والرد على العملاء ومتابعتهم",
+      img: "./asset/serv/ads-1.png",
+    },
+    {
+      serv: "فريق التحليل والتتبع",
+      des: "يعتبر من أهم فرق سيستم أزرق بالنسبة لك و لمتجرك الالكتروني ومهمته هنا هي فحص صفحات الموقع بنسبة 100% ان صفحات الموقع شيقة وجذابة ومرنة للزائر , بحيث يضمن تجربة جيدة لزوار الموقع",
+      img: "./asset/serv/ads-1.png",
+    },
+  ],
+  clients: ["", "", "", "", "", "", "", "", "", ""],
+  teams: [
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+    {
+      img: "",
+      name: "",
+      position: "",
+    },
+  ],
+};
+document.title = data.siteName;
+let partner = document.querySelector(".partner");
+let result = document.querySelector(".result");
+let navebtn = document.querySelector(".nave-btn");
+let nave = document.querySelector(".nave");
+const open = () => {
+  nave.style.width = "350px";
+  navebtn.style.marginRight = "300px";
+  navebtn.style.transform = "rotate(180deg)";
+
+  navebtn.onclick = () => {
+    close();
+  };
+};
+const close = () => {
+  nave.style.width = "0px";
+  navebtn.style.marginRight = "0px";
+  navebtn.style.transform = "rotate(0deg)";
+
+  navebtn.onclick = () => {
+    open();
+  };
+};
+navebtn.onclick = () => {
+  open();
+};
+
+const create = (imgsrc) => {
+  let div = document.createElement("div");
+  let a = document.createElement("a");
+  let img = document.createElement("img");
+  div.classList.add("res", "center", "w-100");
+  div.dataset.aos = "fade-up";
+  a.classList.add("w-100", "center");
+  img.classList.add("w-100");
+  a.href = "#";
+  img.src = imgsrc;
+  a.appendChild(img);
+  div.appendChild(a);
+  return div;
+};
+
+data.partners.map((imgsrc) => {
+  partner.appendChild(create(imgsrc));
+});
+data.results.map((imgsrc) => {
+  result.appendChild(create(imgsrc));
+});
+
 let ul = document.querySelector(".nave-middel");
-let sec = document.querySelectorAll(".sec");
-console.log(sec);
 let list = [
-  ["الرئيسية", "#home"],
-  ["شركائنا", "#partners"],
-  ["نتائج عملائنا", "#result"],
-  ["خطوات الاشتراك", "#steps"],
-  ["خدماتنا", "#service"],
-  ["عملائنا", "#client"],
-  ["فريقنا", "#team"],
+  ["الرئيسية", "#home", "fa-house"],
+  ["شركائنا", "#partners", "fa-handshake"],
+  ["نتائج عملائنا", "#result", "fa-square-poll-vertical"],
+  ["خطوات الاشتراك", "#steps", "fa-backward-step"],
+  ["خدماتنا", "#service", "fa-gears"],
+  ["عملائنا", "#client", "fa-users"],
+  ["فريقنا", "#team", "fa-people-group"],
 ];
 list.map((li) => {
   let lis = document.createElement("li");
   let a = document.createElement("a");
+  let i = document.createElement("i");
+
   lis.classList.add("center", "w-100");
-  a.classList.add("center", "a-not-active", "w-100");
+  a.classList.add("center", "a-not-active", "w-100", "justify-content-end");
+  i.classList.add("fa-solid", li[2], "m-l-40", "m-r-40");
   a.innerText = li[0];
+  a.appendChild(i);
   a.href = li[1];
   a.onclick = () => {
     select(list.indexOf(li));
@@ -27,10 +298,8 @@ var a = document.querySelectorAll(".a-not-active");
 
 a[0].classList.add("active");
 const select = (id) => {
-  for (let i = 0; i < a?.length; i++) {
+  for (let i = 0; i < a.length; i++) {
     a[i].classList.remove("active");
-    sec[i].classList.remove("sec-active");
   }
   a[id].classList.add("active");
-  sec[id].classList.add("sec-active");
 };
